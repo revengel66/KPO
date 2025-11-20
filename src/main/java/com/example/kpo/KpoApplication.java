04eff8ac4e589dc902015c01ca907eb55f7efaec
Natalia <natalia200218@yandex.ru>
2025-11-20 13:41:49 +0400

    бесполезный коммит

--- src/main/java/com/example/kpo/KpoApplication.java
+++ src/main/java/com/example/kpo/KpoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KpoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KpoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KpoApplication.class, args);
+    }
 
 }
